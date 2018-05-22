354a0b519658199a4cc424f27b3389f61a185e90
Elana Ryznar <eryzna@gmail.com>
2018-05-22 06:38:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,8 @@ def adding_matz
         }
      }
 programmer_hash [:yukihiro_matsumoto] [:known_for]="Ruby"
-programmer_hash [:yukihiro_matsumoto] [:languages]="LISP","C"#known_for=>"Ruby"
+programmer_hash [:yukihiro_matsumoto] [:languages]="LISP","C"
+programmer_hash#known_for=>"Ruby"
 
 #programmer_hash [yukihiro_matsumoto] languages =>"LISP","C"
 #   :known_for => "Ruby",
